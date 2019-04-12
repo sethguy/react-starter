b777b08b7b5e071e4a4f7151f856145e1ea68a9f
seth-leap <sterry@leaptodigital.com>
2019-04-12 15:21:02 -0400

    ok got main window

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,31 +1,64 @@
-import React, {Component} from 'react';
-import {loadEvents} from './actions'
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { loadEvents } from './actions'
+import { connect } from 'react-redux';
+
+import './home.css'
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showContextMenu: false,
+            contextMenuPosition: { x: 0, y: 0 }
+        }
+    }
     componentDidMount() {
         this
             .props
             .loadEvents({})
+
+       // document.body.addEventListener('mousedown', this.closeContextMenu)
+    }
+    closeContextMenu = () => {
+
+        if (this.state.showContextMenu) {
+            this.setState({
+                showContextMenu: false,
+                contextMenuPosition: { x: 0, y: 0 }
+            })
+        }
+    }
+    openContextMenu = (event) => {
+
+        this.setState({
+            showContextMenu: true,
+            contextMenuPosition: { x: event.x || 0, y: event.y || 0 }
+        })
+    }
+    onContextMenuClick = (event) => {
+        event.preventDefault()
+        console.log("TCL: Home -> render -> event", event)
+        // nice trick //const positions = Object.keys(event).filter(key => key.indexOf('Y') > -1 || key.indexOf('X') > -1)
+        const { screenX, screenY, clientX, clientY, pageX, pageY, movementX, movementY } = event
+        console.log("TCL: Home -> render -> { screenX, screenY, clientX, clientY, pageX, pageY, movementX, movementY} ", { screenX, screenY, clientX, clientY, pageX, pageY, movementX, movementY })
+        
+        this.openContextMenu({
+            x:clientX,
+            y:clientY,
+        })
+   
     }
     render() {
         return (
-            <div className="h-100" onContextMenu={(event)=>{
-			console.log("TCL: Home -> render -> event", event)
-
-
+            <div className="h-100" onContextMenu={this.onContextMenuClick} >
+                {this.state.showContextMenu && <div 
+                    style={{
+                        top: `${this.state.contextMenuPosition.y}px`,
+                        left: `${this.state.contextMenuPosition.x}px`,
+                    }}
                 
-            }} >
-                <div>
-                    <p>
-                        HOme
-                    </p>
-                </div>
-                <p>
-                    {this.props.eventData.name}
-                </p>
+                className="context-menu border"> faske </div>}
             </div>
-
         );
     }
 }
@@ -38,4 +71,4 @@ const mapStateToProps = ({
     eventData: events.eventData || defaultEvent
 })
 
-export default connect(mapStateToProps, {loadEvents})(Home)
+export default connect(mapStateToProps, { loadEvents })(Home)
